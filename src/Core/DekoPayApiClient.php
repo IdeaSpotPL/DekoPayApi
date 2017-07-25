1c8921ac71f18c9f2101fc7f863f2425d6fbbeb9
SergiuszN <serg.ni2011@gmail.com>
2017-07-25 17:59:32 +0200

    add all request to DekoPayApiClient

--- src/Core/DekoPayApiClient.php
+++ src/Core/DekoPayApiClient.php
@@ -2,11 +2,19 @@
 
 namespace IdeaSpot\DekoPayApi\Core;
 
+use IdeaSpot\DekoPayApi\Requests\AmendmentRequest;
+use IdeaSpot\DekoPayApi\Requests\CancellationRequest;
 use IdeaSpot\DekoPayApi\Requests\CreditApplicationInitialisationRequest;
 use IdeaSpot\DekoPayApi\Requests\CreditApplicationResumeLinkRequest;
+use IdeaSpot\DekoPayApi\Requests\FulfilmentRequest;
+use IdeaSpot\DekoPayApi\Requests\ResendCsnRequest;
 use IdeaSpot\DekoPayApi\Requests\StatusRequest;
+use IdeaSpot\DekoPayApi\Response\AmendmentResponse;
+use IdeaSpot\DekoPayApi\Response\CancellationResponse;
 use IdeaSpot\DekoPayApi\Response\CreditApplicationInitialisationResponse;
 use IdeaSpot\DekoPayApi\Response\CreditApplicationResumeLinkResponse;
+use IdeaSpot\DekoPayApi\Response\FulfilmentResponse;
+use IdeaSpot\DekoPayApi\Response\ResendCsnResponse;
 use IdeaSpot\DekoPayApi\Response\StatusResponse;
 
 class DekoPayApiClient
@@ -47,6 +55,46 @@ class DekoPayApiClient
     public function creditApplicationResumeLink(CreditApplicationResumeLinkRequest $request)
     {
         return new CreditApplicationResumeLinkResponse($this->createRequest($request));
+    }
+
+    /**
+     * @param FulfilmentRequest $request
+     * @return FulfilmentResponse $response
+     * @throws \Exception
+     */
+    public function fullFillApplication(FulfilmentRequest $request)
+    {
+        return new FulfilmentResponse($this->createRequest($request));
+    }
+
+    /**
+     * @param ResendCsnRequest $request
+     * @return ResendCsnResponse $response
+     * @throws \Exception
+     */
+    public function resendCsn(ResendCsnRequest $request)
+    {
+        return new ResendCsnResponse($this->createRequest($request));
+    }
+
+    /**
+     * @param CancellationRequest $request
+     * @return CancellationResponse $response
+     * @throws \Exception
+     */
+    public function cancellation(CancellationRequest $request)
+    {
+        return new CancellationResponse($this->createRequest($request));
+    }
+
+    /**
+     * @param AmendmentRequest $request
+     * @return AmendmentResponse $response
+     * @throws \Exception
+     */
+    public function amendment(AmendmentRequest $request)
+    {
+        return new AmendmentResponse($this->createRequest($request));
     }
 
     private function createRequest(RequestInterface $request)
